7b506f85449e12820bd2d37630542c6be175c7ee
Romans Malinovskis <me@nearly.guru>
2025-11-10 02:14:27 +0000

    implement vantage types

---
+++ vantage-types/persistence/src/lib.rs
@@ -0,0 +1,74 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse::Parse, parse::ParseStream, parse_macro_input, Data, DeriveInput, Fields, Ident};
+
+struct PersistenceArgs {
+    type_name: Ident,
+}
+
+impl Parse for PersistenceArgs {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let type_name: Ident = input.parse()?;
+        Ok(PersistenceArgs { type_name })
+    }
+}
+
+#[proc_macro_attribute]
+pub fn persistence(args: TokenStream, input: TokenStream) -> TokenStream {
+    let args = parse_macro_input!(args as PersistenceArgs);
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = &input.ident;
+
+    let persistence_type = &args.type_name;
+    let type_lower = persistence_type.to_string().to_lowercase();
+
+    let fields = match &input.data {
+        Data::Struct(data_struct) => match &data_struct.fields {
+            Fields::Named(fields) => &fields.named,
+            _ => panic!("Persistence only supports structs with named fields"),
+        },
+        _ => panic!("Persistence only supports structs"),
+    };
+
+    let any_type = quote::format_ident!("Any{}", persistence_type);
+    let trait_name = quote::format_ident!("{}Persistence", persistence_type);
+    let to_method = quote::format_ident!("to_{}_map", type_lower);
+    let from_method = quote::format_ident!("from_{}_map", type_lower);
+
+    let field_insertions = fields.iter().map(|field| {
+        let field_name = field.ident.as_ref().unwrap();
+        let field_name_str = field_name.to_string();
+        quote! {
+            map.insert(#field_name_str.to_string(), crate::#any_type::new(self.#field_name.clone()));
+        }
+    });
+
+    let field_extractions = fields.iter().map(|field| {
+        let field_name = field.ident.as_ref().unwrap();
+        let field_name_str = field_name.to_string();
+        let field_type = &field.ty;
+        quote! {
+            #field_name: map.get(#field_name_str)?.try_get::<#field_type>()?
+        }
+    });
+
+    let expanded = quote! {
+        #input
+
+        impl #trait_name for #name {
+            fn #to_method(&self) -> indexmap::IndexMap<String, crate::#any_type> {
+                let mut map = indexmap::IndexMap::new();
+                #(#field_insertions)*
+                map
+            }
+
+            fn #from_method(map: indexmap::IndexMap<String, crate::#any_type>) -> Option<Self> {
+                Some(Self {
+                    #(#field_extractions),*
+                })
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
---
+++ vantage-types/src/lib.rs
@@ -0,0 +1,5 @@
+// Re-export proc-macro from persistence subcrate
+pub use vantage_types_persistence::persistence;
+
+// Include type_system module with regular macros
+pub mod type_system;
