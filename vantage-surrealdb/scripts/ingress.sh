27cd4b9eba8ba560945305815578c759c4c446a8
Romans Malinovskis <me@nearly.guru>
2026-04-03 03:08:39 +0100

    ingress files more cleanly

--- vantage-surrealdb/scripts/ingress.sh
+++ vantage-surrealdb/scripts/ingress.sh
@@ -4,7 +4,8 @@ source .env
 for db_file in db/*.surql; do
   db_name=$(basename "$db_file" .surql)
   echo "Processing $db_name..."
-  echo "REMOVE DATABASE $db_name;" | surreal sql --endpoint "$DB_ENDPOINT" --username "$DB_USER" --password "$DB_PASS" --auth-level "$DB_AUTH_LEVEL" --ns "$DB_NS" --hide-welcome --json 2>/dev/null || true
+  echo "REMOVE DATABASE IF EXISTS $db_name;" | surreal sql --endpoint "$DB_ENDPOINT" --username "$DB_USER" --password "$DB_PASS" --auth-level "$DB_AUTH_LEVEL" --ns "$DB_NS" --hide-welcome --json 2>/dev/null || true
   echo "DEFINE DATABASE $db_name;" | surreal sql --endpoint "$DB_ENDPOINT" --username "$DB_USER" --password "$DB_PASS" --auth-level "$DB_AUTH_LEVEL" --ns "$DB_NS" --hide-welcome --json
-  cat "$db_file" | surreal sql --endpoint "$DB_ENDPOINT" --username "$DB_USER" --password "$DB_PASS" --auth-level "$DB_AUTH_LEVEL" --ns "$DB_NS" --db "$db_name" --hide-welcome
+  surreal sql --endpoint "$DB_ENDPOINT" --username "$DB_USER" --password "$DB_PASS" --auth-level "$DB_AUTH_LEVEL" --ns "$DB_NS" --db "$db_name" --hide-welcome --multi < "$db_file" > /dev/null
+  echo "  done."
 done
