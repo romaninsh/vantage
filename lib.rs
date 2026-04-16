7fedd6dbec16128a236bb0a19cbfc7ad76f68b4b
romaninsh <romaninsh@users.noreply.github.com>
2026-04-16 22:05:48 +0000

    deploy: f6d6bd665204640faf2d2a748eef064dc0b73dcb

---
+++ lib.rs
@@ -0,0 +1,6 @@
+// Dummy crate so rustdoc-links can resolve items from dependencies.
+pub use vantage_expressions;
+pub use vantage_mongodb;
+pub use vantage_sql;
+pub use vantage_table;
+pub use vantage_types;
