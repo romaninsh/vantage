8c1011acd7ff83503c512055b8cfcef29a91b68e
romaninsh <romaninsh@users.noreply.github.com>
2026-07-15 14:55:08 +0000

    deploy: c91c98b73f064ece36fa8748367a7c664fe4693b

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0df4a5b3.js',
+                path_to_root + 'searchindex-5e583a43.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
